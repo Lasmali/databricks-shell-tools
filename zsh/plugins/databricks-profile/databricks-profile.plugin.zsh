9dbe89da7189d48cc5d49cd21a11919081e7b27e
Lasse Malm Lidegaard <lasse.lidegaard@lego.com>
2024-10-01 21:45:28 +0200

    add selection command

--- zsh/plugins/databricks-profile/databricks-profile.plugin.zsh
+++ zsh/plugins/databricks-profile/databricks-profile.plugin.zsh
@@ -13,6 +13,35 @@ function _reset_databricks_profile_session() {
 }
 
 
-function dps() {
+function dbp_reset() {
   _reset_databricks_profile_session
+}
+
+
+function _list_available_databricks_profiles() {
+
+  BLUE="34"
+  BOLDBLUE="\e[1;${BLUE}m"
+  ENDCOLOR="\e[0m"
+
+  # Present the profiles to the user
+  echo "Available Databricks profiles:"
+
+  profiles=($(grep '^\[.*\]$' ~/.databrickscfg | sed 's/[][]//g'))
+  i=1
+  for profile in "${profiles[@]}"; do
+    echo "$i. ${BOLDBLUE}$profile${ENDCOLOR}"
+    i=$((i+1))
+  done
+
+
+  # Read user selection
+  read "selected_profile?Choose a number: "
+
+  export DATABRICKS_CONFIG_PROFILE=${profiles[$selected_profile]}
+}
+
+
+function dbp_profiles() {
+  _list_available_databricks_profiles
 }
